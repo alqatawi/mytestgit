ad2353dc73e9e4b6d9ea09170d467948450a0e5c
AL_Qatawi <alqatawih@gmail.com>
2022-12-29 13:27:08 +0200

    Change 1 commit

--- app/src/main/java/com/h_alqatawi/mytestgit/MainActivity.kt
+++ app/src/main/java/com/h_alqatawi/mytestgit/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("THIS IS CONTENT IN MY SECOND COMMIT ")
         println("LOCAL CHANGE")
-        println("Experimental")
+        println("CHANGE 1")
 
     }
 }
